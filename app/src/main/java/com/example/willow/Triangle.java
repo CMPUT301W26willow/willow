37a03578b2344723fcfbd4fa0fbba0ae49cf2571
asmarakhtar <asmar@ualberta.ca>
2026-01-30 14:38:08 -0700

    Add Triangle  model extending Shape

---
+++ app/src/main/java/com/example/willow/Triangle.java
@@ -0,0 +1,30 @@
+package com.example.willow;
+
+public class Triangle extends Shape {
+
+    private Integer base;
+    private Integer height;
+
+    public Triangle(Integer x, Integer y, Integer base, Integer height) {
+        super(x, y);          // initialize Shape's x and y
+        this.base = base;
+        this.height = height;
+    }
+
+    public Integer getBase() {
+        return base;
+    }
+
+    public void setBase(Integer base) {
+        this.base = base;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+}
